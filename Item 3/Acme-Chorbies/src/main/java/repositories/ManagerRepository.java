9b2e53c25583ee4aea1587bebcd384367c32a393
Shelux8 <shelux1987@gmail.com>
2017-04-25 22:46:57 +0200

    Repositorio Manager con queries C1 y C2

--- Item 3/Acme-Chorbies/src/main/java/repositories/ManagerRepository.java
+++ Item 3/Acme-Chorbies/src/main/java/repositories/ManagerRepository.java
@@ -1,6 +1,8 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,4 +14,13 @@ public interface ManagerRepository extends JpaRepository<Manager, Integer> {
 
 	@Query("select m from Manager m where m.userAccount.id = ?1")
 	Manager findByUserAccountId(int userAccountId);
+
+	//C1: A listing of managers sorted by the number of events that they organise.
+
+	@Query("select count(e),e.manager.name from Event e group by e.manager")
+	Collection<Object[]> findByNumberEvents();
+
+	//C2: A listing of managers that includes the amount that they due in fees.
+	@Query("select m, m.fee from Manager m;")
+	Collection<Object[]> findByAmountFee();
 }
