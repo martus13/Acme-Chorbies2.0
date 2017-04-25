60d703aac2a577f87c5bf0a24218e707db4cca09
Shelux8 <shelux1987@gmail.com>
2017-04-25 22:26:50 +0200

    Servicio manager, credit cards para managers

--- Item 3/Acme-Chorbies/src/main/java/services/ManagerService.java
+++ Item 3/Acme-Chorbies/src/main/java/services/ManagerService.java
@@ -1,12 +1,15 @@
 
 package services;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import repositories.ManagerRepository;
+import security.Authority;
 import security.LoginService;
 import security.UserAccount;
 import domain.Manager;
@@ -52,4 +55,51 @@ public class ManagerService {
 
 		return result;
 	}
+
+	public Manager findOne(final int managerId) {
+		Assert.isTrue(managerId != 0);
+
+		Manager result;
+
+		result = this.managerRepository.findOne(managerId);
+
+		return result;
+	}
+
+	public Collection<Manager> findAll() {
+		Collection<Manager> result;
+
+		result = this.managerRepository.findAll();
+
+		return result;
+	}
+
+	public Manager create() {
+		Manager result;
+		UserAccount userAccount;
+		Authority authority;
+
+		result = new Manager();
+
+		userAccount = new UserAccount();
+		authority = new Authority();
+
+		authority.setAuthority("MANAGER");
+		userAccount.getAuthorities().add(authority);
+
+		result.setUserAccount(userAccount);
+		result.setFee(0.0);
+
+		return result;
+	}
+
+	public Manager save(final Manager manager) {
+		Assert.notNull(manager);
+
+		Manager result;
+		result = this.managerRepository.save(manager);
+
+		return result;
+	}
+
 }
